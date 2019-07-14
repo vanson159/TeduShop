8871b11d29fa428c4d577f6215d3a6da6dd8a562
vanson_bitbucket <nguyenthaisonpo155@gmail.com>
2019-07-15 05:10:06 +0700

    Commit migrations and reponsitory

---
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+using System.Linq;
+
+namespace TeduShop.Data.Reponsetories
+{
+    public interface IProductCategoryReponsitory
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryReponsitory
+    {
+       
+        public ProductCategoryRepository(DbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
