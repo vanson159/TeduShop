44dad62e730d4d8613e90e014722b281725955a2
vanson_bitbucket <nguyenthaisonpo155@gmail.com>
2019-07-21 05:14:12 +0700

    Xay dung Repository

---
+++ TeduShop.Data/Repositories/FooterRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Inf
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IFooterRepository
+    {
+
+    }
+    public class FooterRepository : 
+    {
+    }
+}
